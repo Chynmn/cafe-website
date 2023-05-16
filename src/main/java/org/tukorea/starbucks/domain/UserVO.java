0925aa03008aab6582ba2d8c48bea76e03b92ceb
chynmn <chynmn0523@gmail.com>
2023-05-17 00:47:44 +0900

    feat: UserVO

--- src/main/java/org/tukorea/starbucks/domain/UserVO.java
+++ src/main/java/org/tukorea/starbucks/domain/UserVO.java
@@ -1,4 +1,17 @@
 package org.tukorea.starbucks.domain;
 
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
 public class UserVO {
+    private int num;
+    private String id;
+    private String passwd;
+    private String userName;
+    private String email;
+    private int point;
+    private Date createdAt;
+    private Date updatedAt;
 }
