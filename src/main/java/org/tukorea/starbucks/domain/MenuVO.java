9f5d3da6d56f9b2c9c3803d59ca85d76cf750864
chynmn <chynmn0523@gmail.com>
2023-05-20 23:38:34 +0900

    feat: MenuVO lombok

--- src/main/java/org/tukorea/starbucks/domain/MenuVO.java
+++ src/main/java/org/tukorea/starbucks/domain/MenuVO.java
@@ -1,4 +1,30 @@
 package org.tukorea.starbucks.domain;
 
+import lombok.Data;
+
+@Data
 public class MenuVO {
+    private Long id;
+
+    private String menuName;
+
+    private String content;
+
+    private String nutritionInformation;
+
+    private int kcal;
+
+    private int saturatedFat;
+
+    private int protein;
+
+    private int natrium;
+
+    private int sugars;
+
+    private int caffeine;
+
+    private String allergy;
+
+    private String image;
 }
