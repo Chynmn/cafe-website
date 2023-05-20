fecabff7d6c92875d352db22734728228a5a991c
chynmn <chynmn0523@gmail.com>
2023-05-20 23:39:53 +0900

    feat: MenuDAO

--- src/main/java/org/tukorea/starbucks/persistence/MenuDAO.java
+++ src/main/java/org/tukorea/starbucks/persistence/MenuDAO.java
@@ -1,4 +1,26 @@
 package org.tukorea.starbucks.persistence;
 
+import org.tukorea.starbucks.domain.MenuVO;
+import org.tukorea.starbucks.domain.ReviewVO;
+
+import java.util.List;
+
 public interface MenuDAO {
+
+    // 메뉴 리스트
+    public List<MenuVO> readMenuList() throws Exception;
+    // 메뉴 등록
+    public void menuInsert(MenuVO menu) throws Exception;
+    // 메뉴 보기
+    public MenuVO menuRead(Long num) throws Exception;
+    // 메뉴 수정
+    public void menuUpdate(MenuVO menu) throws Exception;
+    // 메뉴 삭제
+    public void menuDelete(Long num) throws Exception;
+    // 리뷰 리스트
+    public List<ReviewVO> readReviewList(Long num) throws Exception;
+    // 리뷰 등록
+    public void reviewInsert(ReviewVO review) throws Exception;
+    // 리뷰 삭제
+    public void reviewDelete(int num) throws Exception;
 }
