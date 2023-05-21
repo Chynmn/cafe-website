9ce4cd217dfb5ab37cba78b50b8d41b4c9e5a533
chynmn <chynmn0523@gmail.com>
2023-05-21 23:29:17 +0900

    refactor: type modify
    Long -> int

--- src/main/java/org/tukorea/starbucks/domain/MenuVO.java
+++ src/main/java/org/tukorea/starbucks/domain/MenuVO.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class MenuVO {
-    private Long id;
+    private int id;
 
     private String menuName;
 
--- src/main/java/org/tukorea/starbucks/domain/UserVO.java
+++ src/main/java/org/tukorea/starbucks/domain/UserVO.java
@@ -5,7 +5,7 @@ import lombok.Data;
 @Data
 public class UserVO {
 
-    private Long num;
+    private int num;
 
     private String id;
 
