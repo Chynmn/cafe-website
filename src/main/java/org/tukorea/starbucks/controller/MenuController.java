aa8803c0484ca15b26187915df1e3581bb00b1a5
chynmn <chynmn0523@gmail.com>
2023-05-26 07:46:07 +0900

    refactor: MenuController

--- src/main/java/org/tukorea/starbucks/controller/MenuController.java
+++ src/main/java/org/tukorea/starbucks/controller/MenuController.java
@@ -5,14 +5,12 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.tukorea.starbucks.domain.MenuVO;
-import org.tukorea.starbucks.domain.ReviewVO;
 import org.tukorea.starbucks.service.MenuService;
 import org.tukorea.utils.UploadFileUtils;
 
@@ -22,7 +20,7 @@ import java.io.File;
 import java.util.List;
 
 @Controller
-@RequestMapping(value = "/starbucks")
+@RequestMapping(value = "/menu")
 public class MenuController {
     private static final Logger logger = LoggerFactory.getLogger(MenuController.class);
 
@@ -34,15 +32,14 @@ public class MenuController {
 
     // 전체 메뉴 뷸러오기
     @RequestMapping(value = "/menu", method = RequestMethod.GET)
-    public List<MenuVO> menuGet(Model model, HttpServletRequest request) throws Exception {
+    public String menuGet(Model model, HttpServletRequest request) throws Exception {
 
         List<MenuVO> menu = menuService.menuList();
         HttpSession session = request.getSession();
         model.addAttribute("menu", menu); // 근데 이 모델 어트리뷰트는 무슨 역할인가? JSP에 뿌려줄겨
-        model.addAttribute("id", session.getAttribute("id"));
+//        model.addAttribute("id", session.getAttribute("id"));
 //        model.addAttribute("imageFile", session.getAttribute("imageFile"));
-//        return "starbucks/menu";
-        return menu;
+        return "menu/menu";
     }
 
     // 메뉴 등록 페이지 이동
@@ -85,7 +82,7 @@ public class MenuController {
     }
 
     // 메뉴 수정 페이지 이동
-    @RequestMapping(value = "/modify", method = RequestMethod.GET)
+    @RequestMapping(value = "/menu_modify", method = RequestMethod.GET)
     public String menuModifyGet(@RequestParam("num") int num, Model model) throws Exception {
         MenuVO menu = menuService.menuRead(num);
         menuService.menuUpdate(menu);
@@ -93,28 +90,28 @@ public class MenuController {
         return "starbucks/menu_modify";
     }
 
-    // 메뉴 수정 기능 실행
-    @RequestMapping(value = "/modify", method = RequestMethod.POST)
-    public String menuModifyPost(@ModelAttribute("menu") MenuVO menu, RedirectAttributes rttr) throws Exception {
-        menuService.menuUpdate(menu);
-//        rttr.addAttribute("residence",board.getResidence());
-//        rttr.addAttribute("residenceGu",board.getResidenceGu());
-        return "redirect:starbucks/menu";
-    }
-
-    // 리뷰 등록
-    @RequestMapping(value = "/review", method = RequestMethod.POST)
-    public String reviewPost(@ModelAttribute("comment") ReviewVO review, RedirectAttributes rttr) throws Exception {
-        menuService.reviewInsert(review);
-
-        return "redirect:starbucks/menu";
-    }
-
-    // 리뷰 삭제
-    @RequestMapping(value = "/reviewdelete", method = RequestMethod.GET)
-    public String reviewDeleteGet(@ModelAttribute("review") ReviewVO review , RedirectAttributes rttr) throws Exception {
-        menuService.reviewDelete(review.getReviewNum());
-
-        return "redirect:starbucks/menu";
-    }
+//    // 메뉴 수정 기능 실행
+//    @RequestMapping(value = "/menu_modify", method = RequestMethod.POST)
+//    public String menuModifyPost(@ModelAttribute("menu") MenuVO menu, RedirectAttributes rttr) throws Exception {
+//        menuService.menuUpdate(menu);
+////        rttr.addAttribute("residence",board.getResidence());
+////        rttr.addAttribute("residenceGu",board.getResidenceGu());
+//        return "redirect:starbucks/menu";
+//    }
+//
+//    // 리뷰 등록
+//    @RequestMapping(value = "/review", method = RequestMethod.POST)
+//    public String reviewPost(@ModelAttribute("comment") ReviewVO review, RedirectAttributes rttr) throws Exception {
+//        menuService.reviewInsert(review);
+//
+//        return "redirect:starbucks/menu";
+//    }
+//
+//    // 리뷰 삭제
+//    @RequestMapping(value = "/review_delete", method = RequestMethod.GET)
+//    public String reviewDeleteGet(@ModelAttribute("review") ReviewVO review , RedirectAttributes rttr) throws Exception {
+//        menuService.reviewDelete(review.getReviewNum());
+//
+//        return "redirect:starbucks/menu";
+//    }
 }
