b2eefca8d05f20cf4a6e06b2ff5da2481a92702c
chynmn <chynmn0523@gmail.com>
2023-05-26 07:45:30 +0900

    refactor: MenuDAO

--- src/main/java/org/tukorea/starbucks/persistence/MenuDAO.java
+++ src/main/java/org/tukorea/starbucks/persistence/MenuDAO.java
@@ -1,26 +1,26 @@
 package org.tukorea.starbucks.persistence;
 
 import org.tukorea.starbucks.domain.MenuVO;
-import org.tukorea.starbucks.domain.ReviewVO;
 
 import java.util.List;
 
 public interface MenuDAO {
 
     // 메뉴 리스트
-    public List<MenuVO> readMenuList() throws Exception;
+    public List<MenuVO> readList() throws Exception;
     // 메뉴 등록
-    public void menuInsert(MenuVO menu) throws Exception;
+    public void insert(MenuVO menu) throws Exception;
     // 메뉴 보기
-    public MenuVO menuRead(int num) throws Exception;
+    public MenuVO read(int id) throws Exception;
     // 메뉴 수정
-    public void menuUpdate(MenuVO menu) throws Exception;
+    public void update(MenuVO menu) throws Exception;
     // 메뉴 삭제
-    public void menuDelete(int num) throws Exception;
-    // 리뷰 리스트
-    public List<ReviewVO> readReviewList(int reviewNum) throws Exception;
-    // 리뷰 등록
-    public void reviewInsert(ReviewVO review) throws Exception;
-    // 리뷰 삭제
-    public void reviewDelete(int reviewNum) throws Exception;
+    public void delete(int id) throws Exception;
+    public void deleteToAddTransaction(int id, MenuVO menu) throws Exception;
+//    // 리뷰 리스트
+//    public List<ReviewVO> readReviewList(int reviewNum) throws Exception;
+//    // 리뷰 등록
+//    public void reviewInsert(ReviewVO review) throws Exception;
+//    // 리뷰 삭제
+//    public void reviewDelete(int reviewNum) throws Exception;
 }
--- src/main/java/org/tukorea/starbucks/persistence/MenuDAOImpl.java
+++ src/main/java/org/tukorea/starbucks/persistence/MenuDAOImpl.java
@@ -2,14 +2,16 @@ package org.tukorea.starbucks.persistence;
 
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Isolation;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import org.tukorea.starbucks.domain.MenuVO;
-import org.tukorea.starbucks.domain.ReviewVO;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
+@Repository
 public class MenuDAOImpl implements MenuDAO {
     @Autowired
     private SqlSession sqlSession;
@@ -17,63 +19,67 @@ public class MenuDAOImpl implements MenuDAO {
     private static final String namespace = "org.tukorea.starbucks.mapper.MenuMapper";
 
     @Override
-    public List<MenuVO> readMenuList() throws Exception {
+    public List<MenuVO> readList() throws Exception {
         // TODO Auto-generated method stub
         List<MenuVO> menulist = new ArrayList<MenuVO>();
-        Map<String, Object> map = new HashMap<>();
-//        map.put("id", id);
-        menulist = sqlSession.selectList(namespace+".menu_List", map);
+        menulist = sqlSession.selectList(namespace+".selectAll");
 
         return menulist;
     }
 
     @Override
-    public void menuInsert(MenuVO menu) throws Exception {
+    public void insert(MenuVO menu) throws Exception {
         // TODO Auto-generated method stub
-        sqlSession.insert(namespace+".menuInsert", menu);
+        sqlSession.insert(namespace+".insert", menu);
     }
 
     @Override
-    public MenuVO menuRead(int num) throws Exception {
+    public MenuVO read(int id) throws Exception {
         // TODO Auto-generated method stub
-        MenuVO menuRead = sqlSession.selectOne(namespace+".menuRead", num);
+        MenuVO menuRead = sqlSession.selectOne(namespace+".read", id);
 
         return menuRead;
     }
 
     @Override
-    public void menuUpdate(MenuVO menu) throws Exception {
+    public void update(MenuVO menu) throws Exception {
         // TODO Auto-generated method stub
-        sqlSession.update(namespace+".menuUpdate", menu);
+        sqlSession.update(namespace+".update", menu);
     }
 
-    @Override
-    public void menuDelete(int num) throws Exception {
+    @Transactional(propagation= Propagation.REQUIRED, isolation= Isolation.READ_COMMITTED, timeout = 10)
+    public void delete(int id) throws Exception {
         // TODO Auto-generated method stub
-        sqlSession.delete(namespace+".menuDelete", num);
+        sqlSession.delete(namespace+".delete", id);
     }
 
-    @Override
-    public List<ReviewVO> readReviewList(int reviewNum) throws Exception {
-        // TODO Auto-generated method stub
-        List<ReviewVO> reviewList = new ArrayList<ReviewVO>();
-        Map<String, Object> map = new HashMap<>();
-//        map.put("id", id);
-        reviewList = sqlSession.selectList(namespace+".readReviewList", map);
-
-        return reviewList;
+    @Transactional(propagation= Propagation.REQUIRED, isolation= Isolation.READ_COMMITTED, timeout = 10)
+    public void deleteToAddTransaction(int id, MenuVO menu) throws Exception {
+        sqlSession.delete(namespace + ".delete", id);
+        sqlSession.update(namespace + ".insert", menu);
     }
 
-    @Override
-    public void reviewInsert(ReviewVO review) throws Exception {
-        // TODO Auto-generated method stub
-        sqlSession.insert(namespace+".reviewInsert", review);
-    }
-
-    @Override
-    public void reviewDelete(int reviewNum) throws Exception {
-        // TODO Auto-generated method stub
-        sqlSession.delete(namespace+".reviewDelete", reviewNum);
-    }
+//    @Override
+//    public List<ReviewVO> readReviewList(int reviewNum) throws Exception {
+//        // TODO Auto-generated method stub
+//        List<ReviewVO> reviewList = new ArrayList<ReviewVO>();
+//        Map<String, Object> map = new HashMap<>();
+////        map.put("id", id);
+//        reviewList = sqlSession.selectList(namespace+".readReviewList", map);
+//
+//        return reviewList;
+//    }
+//
+//    @Override
+//    public void reviewInsert(ReviewVO review) throws Exception {
+//        // TODO Auto-generated method stub
+//        sqlSession.insert(namespace+".reviewInsert", review);
+//    }
+//
+//    @Override
+//    public void reviewDelete(int reviewNum) throws Exception {
+//        // TODO Auto-generated method stub
+//        sqlSession.delete(namespace+".reviewDelete", reviewNum);
+//    }
 
 }
