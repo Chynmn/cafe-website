25a12b9af8cf7808fb385abb353043af4afdad06
chynmn <chynmn0523@gmail.com>
2023-05-26 07:40:57 +0900

    test: MyBatisTest

--- src/test/java/org/tukorea/starbucks/test/MyBatisTest.java
+++ src/test/java/org/tukorea/starbucks/test/MyBatisTest.java
@@ -1,12 +1,13 @@
 package org.tukorea.starbucks.test;
 
-import javax.inject.Inject;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.inject.Inject;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = { "file:src/main/webapp/WEB-INF/spring/root-context.xml" })
