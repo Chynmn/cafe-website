02b73d416557f1ea665a1d2449ad6dc9434098a3
chynmn <chynmn0523@gmail.com>
2023-05-26 07:44:53 +0900

    refactor: MemberService

--- src/main/java/org/tukorea/starbucks/service/MemberServiceImpl.java
+++ src/main/java/org/tukorea/starbucks/service/MemberServiceImpl.java
@@ -3,7 +3,7 @@ package org.tukorea.starbucks.service;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.tukorea.starbucks.domain.UserVO;
+import org.tukorea.starbucks.domain.MemberVO;
 import org.tukorea.starbucks.persistence.MemberDAO;
 
 @Component
@@ -17,14 +17,29 @@ public class MemberServiceImpl implements MemberService {
     private static final String namespace = "org.tukorea.starbucks.mapper.UserMapper";
 
     @Override
-    public void register(UserVO user) throws Exception {
+    public void register(MemberVO user) throws Exception {
         // TODO Auto-generated method stub
         sqlSession.insert(namespace+".register", user);
     }
 
     @Override
-    public UserVO login(UserVO user) throws Exception {
+    public MemberVO login(MemberVO user) throws Exception {
         // TODO Auto-generated method stub
         return memberDAO.login(user);
+    }
+
+    @Override
+    public void deleteMember(String id) throws Exception {
+        memberDAO.delete(id);
+    }
+
+    @Override
+    public void updateMember(MemberVO member) throws Exception {
+        memberDAO.update(member);
+    }
+
+    @Override
+    public MemberVO readMember(String id) throws Exception {
+        return memberDAO.read(id);
     }
 }
