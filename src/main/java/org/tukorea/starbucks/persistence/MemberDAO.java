87295c4156195c127c27583435d5852743a2b65c
chynmn <chynmn0523@gmail.com>
2023-05-26 07:44:29 +0900

    refactor: MemberDAO

--- src/main/java/org/tukorea/starbucks/persistence/MemberDAO.java
+++ src/main/java/org/tukorea/starbucks/persistence/MemberDAO.java
@@ -1,16 +1,22 @@
 package org.tukorea.starbucks.persistence;
 
 
-import org.tukorea.starbucks.domain.UserVO;
-
-import java.util.List;
+import org.tukorea.starbucks.domain.MemberVO;
 
 public interface MemberDAO {
 
     // 회원가입
-    public void register(UserVO user) throws Exception;
+    public void register(MemberVO user) throws Exception;
 
     // 로그인
-    public UserVO login(UserVO user) throws Exception;
+    public MemberVO login(MemberVO user) throws Exception;
+
+    // 탈퇴
+    public void delete(String id) throws Exception;
+
+    // 회원정보 수정
+    public void update(MemberVO member) throws Exception;
+
+    public MemberVO read(String id) throws Exception;
 
 }
--- src/main/java/org/tukorea/starbucks/persistence/MemberDAOImpl.java
+++ src/main/java/org/tukorea/starbucks/persistence/MemberDAOImpl.java
@@ -3,7 +3,7 @@ package org.tukorea.starbucks.persistence;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.tukorea.starbucks.domain.UserVO;
+import org.tukorea.starbucks.domain.MemberVO;
 
 @Component
 public class MemberDAOImpl implements MemberDAO{
@@ -14,15 +14,30 @@ public class MemberDAOImpl implements MemberDAO{
     private static final String namespace = "org.tukorea.starbucks.mapper.UserMapper";
 
     @Override
-    public void register(UserVO user) throws Exception {
+    public void register(MemberVO user) throws Exception {
         // TODO Auto-generated method stub
         sqlSession.insert(namespace+".register", user);
 
     }
 
     @Override
-    public UserVO login(UserVO user) throws Exception {
+    public MemberVO login(MemberVO user) throws Exception {
         // TODO Auto-generated method stub
         return sqlSession.selectOne(namespace+".login",user);
+    }
+
+    @Override
+    public void delete(String id) throws Exception {
+        sqlSession.delete(namespace + ".delete", id);
+    }
+
+    @Override
+    public void update(MemberVO member) throws Exception {
+        sqlSession.update(namespace + ".update", member);
+    }
+
+    @Override
+    public MemberVO read(String id) throws Exception {
+        return sqlSession.selectOne(namespace + ".selectByid", id);
     }
 }
