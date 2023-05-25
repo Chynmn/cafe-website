136c02f2666c5e59427a05fc333ee7feb29accac
chynmn <chynmn0523@gmail.com>
2023-05-26 07:45:15 +0900

    refactor: MemberController

--- src/main/java/org/tukorea/starbucks/controller/MemberController.java
+++ src/main/java/org/tukorea/starbucks/controller/MemberController.java
@@ -3,9 +3,10 @@ package org.tukorea.starbucks.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import org.tukorea.starbucks.domain.UserVO;
+import org.tukorea.starbucks.domain.MemberVO;
 import org.tukorea.starbucks.service.MemberService;
 
 import javax.servlet.http.HttpServletRequest;
@@ -22,14 +23,15 @@ public class MemberController {
     // 로그인
     @GetMapping(value = "/login")
     public String getlogin() throws Exception{
-        return "/member/login";
+        logger.info("login GET Method executed.");
+        return "member/login";
     }
 
     @PostMapping(value = "/login")
-    public String login(UserVO user, HttpServletRequest req, RedirectAttributes rttr) throws Exception {
-
+    public String login(MemberVO user, HttpServletRequest req, RedirectAttributes rttr) throws Exception {
+        logger.info("login Post Method executed.");
         HttpSession session = req.getSession();
-        UserVO login = memberService.login(user);
+        MemberVO login = memberService.login(user);
         if (login == null) {
             session.setAttribute("member", null);
             rttr.addFlashAttribute("msg", false);
@@ -41,16 +43,16 @@ public class MemberController {
 
     // 회원가입
     @GetMapping(value = { "/register" })
-    public void getRegister() throws Exception {
+    public String getRegister() throws Exception {
         logger.info(" /register URL GET method called. then forward member_register.jsp.");
-
+        return "member/register";
     }
 
     @PostMapping(value = { "/register" })
-    public String createMemberPost(@RequestBody UserVO vo) throws Exception {
+    public String createMemberPost(@RequestBody MemberVO vo) throws Exception {
         memberService.register(vo);
     	
-        return "redirect:/";
+        return "redirect:/login";
     }
 
     // 로그아웃
@@ -60,6 +62,36 @@ public class MemberController {
         return "redirect:/";
     }
 
+    @RequestMapping(value = {"/read"}, method = RequestMethod.GET)
+    public String readMember(@RequestParam("id") String id, Model model) throws Exception {
+        MemberVO member = memberService.readMember(id);
+        logger.info(" /read?id=" + id + " URL called. then readMember method executed.");
+        model.addAttribute("member", member);
 
+        return "member/member_read";
+    }
+
+    @RequestMapping(value = "/modify", method = RequestMethod.GET)
+    public String modifyMemberGet(HttpServletRequest req, Model model) throws Exception {
+        HttpSession session = req.getSession();
+        String id = (String)session.getAttribute("member");
+
+        MemberVO member = memberService.readMember(id);
+
+        logger.info(" /modify?id=" + id + " URL GET method called. then forward member_modify.jsp.");
+        model.addAttribute("member", member);
+
+        return "member/member_modify";
+    }
+
+    @RequestMapping(value = "/modify", method = RequestMethod.POST)
+    public String modifyMemberPost(@ModelAttribute("member") MemberVO vo) throws Exception {
+        memberService.updateMember(vo);
+
+        logger.info(vo.toString());
+        logger.info("/modify?id=" + vo.getId() + " URL POST method called. then modifyMemberPost executed.");
+
+        return "redirect:/mission/list";
+    }
 
 }
